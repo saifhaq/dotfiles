47106026054775c032543c416b47c41085781d58
saif <saif.haq99@gmail.com>
2021-04-27 00:14:03 +0100

    added fix monitors scripts

---
+++ scripts/fixmonitors.sh
@@ -0,0 +1,1 @@
+loadkeys uk | xrandr --output DP-0 --mode 1920x1080 --rate 60 --pos 0x0 --rotate left --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --rate 144 --primary --pos 3640x0 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --rate 144 --pos 1080x0 --rotate normal --output DP-5 --off --output USB-C-0 --off
