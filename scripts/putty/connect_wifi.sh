6e0257d0a50070d93fb920f5674a3d327384c89f
Saif <saif.haq99@gmail.com>
2021-05-27 12:14:07 +0100

    added work two monitors script

---
+++ scripts/putty/connect_wifi.sh
@@ -0,0 +1,1 @@
+nmcli device wifi connect <APNAME> password <PASSWORD>
--- scripts/putty/work_two_monitors.sh
+++ scripts/putty/work_two_monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 3840x2160 --pos 7680x0 --rotate normal --output DP1 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP2 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output eDP1 --primary  --pos 7680x0 --rotate normal --output DP1 --mode 3840x2160 --pos 3840x0 --rotate normal --output DP2 --mode 3840x2160 --pos 0x0 --rotate normal --output VIRTUAL1 --off
