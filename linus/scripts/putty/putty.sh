89a36521ee8586cecd5cc0fe4c40fd8102c014c8
saif <saif@putty.localdomain>
2021-12-17 11:43:18 +0000

    added putty dotfiles

---
+++ linus/scripts/putty/putty.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2400 --scale 1.5x1.5 --pos 0x0 --rotate normal
--- scripts/putty/putty_laptop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output VIRTUAL1 --off
